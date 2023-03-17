cb62961a6487ef0cf0785e7a7d07c52f9bba5c9b
most reverend <asante7man7@gmail.com>
2023-03-16 19:14:44 -0700

    nice meeting you

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *
+ * main - Hello, printf
+ *
+ * Return: good 0(GREAT)
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
