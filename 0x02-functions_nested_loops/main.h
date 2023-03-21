dfedec4f4b79b5bb3b59ea273dfca088d8f20b68
most reverend <asante7man7@gmail.com>
2023-03-21 16:50:58 -0700

    Debugging can be boring

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-int_putchar(char val);
-void print_alphatbet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int n);
-int_abs(int);
+int _putchar(char val);
 #endif
