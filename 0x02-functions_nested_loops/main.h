4f260886da0253f4df7674d9fd20fcd5208c5bfd
most reverend <asante7man7@gmail.com>
2023-03-21 15:28:36 -0700

    debugging is fraustrating though

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+int_putchar(char val);
+void print_alphatbet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int_abs(int);
+#endif
